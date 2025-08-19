08b73dc19fc7d933c13694c032756142e36904c2
someone <renhongxu0820@hotmail.com>
2025-08-19 23:15:56 +0800

    Fix: conv

--- source/source_esolver/esolver_ks_pw.cpp
+++ source/source_esolver/esolver_ks_pw.cpp
@@ -620,9 +620,11 @@ void ESolver_KS_PW<T, Device>::iter_finish(UnitCell& ucell, const int istep, int
                 auto start = std::chrono::high_resolution_clock::now();
                 exx_helper.set_firstiter(false);
                 exx_helper.op_exx->first_iter = false;
+                double dexx = exx_helper.cal_exx_energy(this->kspw_psi);
                 exx_helper.set_psi(this->kspw_psi);
+                dexx -= exx_helper.cal_exx_energy(this->kspw_psi);
 
-                conv_esolver = exx_helper.exx_after_converge(iter);
+                conv_esolver = std::abs(dexx) < PARAM.inp.scf_ene_thr || exx_helper.exx_after_converge(iter);
 
                 if (!conv_esolver)
                 {

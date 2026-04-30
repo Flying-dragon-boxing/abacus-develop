d1c59795691b61714aafd23b96e85a242b30acb0
someone <renhongxu0820@hotmail.com>
2026-04-30 12:18:03 +0800

    Relax NCCL discovery for existing environments

--- cmake/SetupNccl.cmake
+++ cmake/SetupNccl.cmake
@@ -1,5 +1,7 @@
 include_guard(GLOBAL)
 
+include(CheckIncludeFileCXX)
+
 function(abacus_setup_nccl target_name)
   find_library(NCCL_LIBRARY NAMES nccl
       HINTS ${NCCL_PATH} ${NVHPC_ROOT_DIR}
@@ -9,19 +11,36 @@ function(abacus_setup_nccl target_name)
       PATHS ${CUDAToolkit_ROOT}
       PATH_SUFFIXES include comm_libs/nccl/include)
 
-  if(NOT NCCL_LIBRARY OR NOT NCCL_INCLUDE_DIR)
+  check_include_file_cxx("nccl.h" HAVE_NCCL_HEADER)
+
+  if(NOT NCCL_LIBRARY)
+    set(NCCL_LIBRARY nccl)
+  endif()
+
+  if(NOT NCCL_INCLUDE_DIR AND NOT HAVE_NCCL_HEADER)
     message(FATAL_ERROR
       "NCCL not found. Set NCCL_PATH or NVHPC_ROOT_DIR.")
   endif()
 
-  message(STATUS "Found NCCL for parallel_device: ${NCCL_LIBRARY}")
+  if(NCCL_INCLUDE_DIR)
+    message(STATUS "Found NCCL for parallel_device: ${NCCL_LIBRARY}")
+  else()
+    message(STATUS "Using default compiler/linker search paths for NCCL: ${NCCL_LIBRARY}")
+  endif()
   if(NOT TARGET NCCL::NCCL)
     add_library(NCCL::NCCL IMPORTED INTERFACE)
-    set_target_properties(NCCL::NCCL PROPERTIES
-        INTERFACE_LINK_LIBRARIES "${NCCL_LIBRARY}"
-        INTERFACE_INCLUDE_DIRECTORIES "${NCCL_INCLUDE_DIR}")
+    if(NCCL_INCLUDE_DIR)
+      set_target_properties(NCCL::NCCL PROPERTIES
+          INTERFACE_LINK_LIBRARIES "${NCCL_LIBRARY}"
+          INTERFACE_INCLUDE_DIRECTORIES "${NCCL_INCLUDE_DIR}")
+    else()
+      set_target_properties(NCCL::NCCL PROPERTIES
+          INTERFACE_LINK_LIBRARIES "${NCCL_LIBRARY}")
+    endif()
   endif()
 
-  include_directories(${NCCL_INCLUDE_DIR})
+  if(NCCL_INCLUDE_DIR)
+    target_include_directories(${target_name} PRIVATE ${NCCL_INCLUDE_DIR})
+  endif()
   target_link_libraries(${target_name} NCCL::NCCL)
 endfunction()
